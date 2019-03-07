2bbfa74cd7a4997a3f3bd54a6d98914cb62a28b6
Sophearak Tha <t.sophearak@gmail.com>
2019-03-07 17:46:05 +0700

    update

--- main__mod__.go
+++ main__mod__.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-  "net/http"
-  "__NOW_HANDLER_PACKAGE_NAME"
+	"net/http"
+	"__NOW_HANDLER_PACKAGE_NAME"
 
-  now "github.com/zeit/now-builders/utils/go/bridge"
+	now "github.com/zeit/now-builders/utils/go/bridge"
 )
 
 func main() {
-  now.Start(http.HandlerFunc(__NOW_HANDLER_FUNC_NAME))
+	now.Start(http.HandlerFunc(__NOW_HANDLER_FUNC_NAME))
 }
